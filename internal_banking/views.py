1c86e799188bba975bf535168427b669a9b58240
Shourov Roy <shourovroy2389@gmail.com>
2025-11-27 23:13:46 +0530

    feat: enhance internal banking control panel with search functionality and display bank details

--- internal_banking/views.py
+++ internal_banking/views.py
@@ -1,7 +1,21 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView, CreateView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from ledger.models import Ledger, Transaction as LedgerTransaction
 # Create your views here.
 
 
-class InternalBankingControlPanelView(TemplateView):
+class InternalBankingControlPanelView(LoginRequiredMixin, ListView):
+    login_url = "/login/"
     template_name = 'internal_banking/control-panel.html'
+    model = Ledger
+    context_object_name = "banks"
+
+    def get_queryset(self):
+
+        query = self.request.GET.get("search_bank", None)
+
+        if query:
+            return self.model.objects.filter(business=self.request.user, account_name__icontains=query).exclude(branch=None)
+
+        return self.model.objects.filter(business=self.request.user).exclude(branch=None)

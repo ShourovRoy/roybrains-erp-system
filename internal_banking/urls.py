526751ededa300aafa19388bf6ec9ab9c4c0dabf
Shourov Roy <shourovroy2389@gmail.com>
2025-11-28 12:07:03 +0530

    feat: implement withdraw functionality with form and view for bank transactions

--- internal_banking/urls.py
+++ internal_banking/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import InternalBankingControlPanelView
+from .views import InternalBankingControlPanelView, InternalBankingWithdraw
 
 urlpatterns = [
     path('internal-banking/control-panel/', InternalBankingControlPanelView.as_view(), name='internal_banking_control_panel'),
+    path('internal-banking/withdraw/<int:bank_id>/', InternalBankingWithdraw.as_view(), name='internal_banking_withdraw'),
 ]
--- internal_banking/views.py
+++ internal_banking/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView, ListView, CreateView
+from django.views.generic import TemplateView, ListView, CreateView, DetailView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from ledger.models import Ledger, Transaction as LedgerTransaction
+from django.shortcuts import get_object_or_404, redirect
+from django.contrib import messages
+from datetime import datetime
+from django.db import transaction
 # Create your views here.
 
 
@@ -19,3 +23,63 @@ class InternalBankingControlPanelView(LoginRequiredMixin, ListView):
             return self.model.objects.filter(business=self.request.user, account_name__icontains=query).exclude(branch=None)
 
         return self.model.objects.filter(business=self.request.user).exclude(branch=None)
+    
+
+class InternalBankingWithdraw(LoginRequiredMixin, CreateView, DetailView):
+    login_url = "/login/"
+    model = LedgerTransaction
+    fields = []
+    context_object_name = "bank_details"
+    template_name = "internal_banking/internal-banking-withdraw.html"
+
+    def get_object(self, queryset = ...):
+
+        bank_id = int(self.kwargs["bank_id"])
+
+        return get_object_or_404(Ledger, business=self.request.user, pk=bank_id)
+    
+
+    def form_valid(self, form):
+
+        bank_id = int(self.kwargs["bank_id"])
+
+        withdraw_amount = float(self.request.POST.get("withdraw_amount", 0.0));
+        date_time = self.request.POST.get("datetime", None)
+
+        if date_time is None:
+            messages.error(request=self.request, message="Date is required!")
+            return self.form_invalid(form);
+    
+
+        try:
+            date_time = datetime.strptime(date_time, "%Y-%m-%dT%H:%M")
+        except Exception as e:
+            print(e)
+            messages.error(request=self.request, message="Invalid date!")
+            return self.form_invalid(form);
+
+
+        if withdraw_amount < 1:
+            messages.warning(request=self.request, message="You can't make empty transaction!")
+            return self.form_invalid(form) 
+
+
+
+        bank_details_ledger = get_object_or_404(Ledger, business=self.request.user, pk=bank_id)
+
+        # if we are withdrawing then it will credit from the bank balance in out book
+        with transaction.atomic():
+            self.model.objects.create(
+                business=self.request.user,
+                ledger=bank_details_ledger,
+                credit=withdraw_amount,
+                debit=0.0,
+                description="withdrawn",
+                date=date_time
+            )
+
+            # TODO: debit the cashbook
+
+            messages.success(request=self.request, message=f"{withdraw_amount} from {bank_details_ledger.account_name} has been withdrawn.")
+            return redirect("internal_banking_withdraw", bank_details_ledger.pk)
+        

272c4a31dd9826568d980f6f8c2a7537c09f74ac
Shourov Roy <shourovroy2389@gmail.com>
2025-12-10 00:26:58 +0600

    feat: Add Cashbook Detail view and template with transaction listing

--- cashbook/urls.py
+++ cashbook/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import CashbookListView
+from .views import CashbookListView, CashbookDetailView
 
 app_name = "cashbook"
 urlpatterns = [
-    path("cashbook-panel/", CashbookListView.as_view(), name="cashbook-control-panel")
+    path("cashbook-panel/", CashbookListView.as_view(), name="cashbook-control-panel"),
+    path("cashbook-panel/cashbook-details/<int:pk>/", CashbookDetailView.as_view(), name="cashbook-detail"),
 ]
--- cashbook/views.py
+++ cashbook/views.py
@@ -1,6 +1,7 @@
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from django.contrib.auth.mixins import LoginRequiredMixin
-from .models import CashBook
+from .models import CashBook, CashTransaction
+from django.shortcuts import get_object_or_404
 # Create your views here.
 
 
@@ -15,4 +16,20 @@ class CashbookListView(LoginRequiredMixin, ListView):
 
     def get_queryset(self):
         return super().get_queryset().filter(business=self.request.user).order_by('-date')
+    
 
+# Cashbook Detail View with transactions list
+class CashbookDetailView(LoginRequiredMixin, DetailView):
+    template_name = "cashbook/cashbook-details.html"
+    login_url = "/login/"
+    model = CashBook
+    context_object_name = "cashbook_details"
+
+
+    def get_object(self, queryset = None):
+        return get_object_or_404(self.model, business=self.request.user, pk=self.kwargs.get('pk'))
+
+    def get_context_data(self, **kwargs):
+        context =  super().get_context_data(**kwargs)
+        context['entries'] = CashTransaction.objects.filter(business=self.request.user, cashbook=self.get_object()).order_by('date')
+        return context

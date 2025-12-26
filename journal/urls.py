044e3135cf8c60f34bf88a931496bfa5bf65ff20
Shourov Roy <shourovroy2389@gmail.com>
2025-12-27 00:09:09 +0600

    feat: Journal Details view with records list added

--- journal/urls.py
+++ journal/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import JournalBookList 
+from .views import JournalBookList, JournalBookDetailsView
 
 
 app_name = "journal"
 urlpatterns = [
-    path("journal-books-list/", JournalBookList.as_view(), name="journal-books-list")
+    path("journal-books-list/", JournalBookList.as_view(), name="journal-books-list"),
+    path("journal-details/<int:pk>/", JournalBookDetailsView.as_view(), name="journal-details")
 ]
--- journal/views.py
+++ journal/views.py
@@ -1,4 +1,5 @@
-from django.views.generic import ListView 
+from django.shortcuts import get_object_or_404
+from django.views.generic import ListView , DeleteView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import redirect 
 from .models import JournalBook, JournalTransaction
@@ -16,4 +17,36 @@ class JournalBookList(LoginRequiredMixin, ListView):
         return super().get_queryset().filter(business=self.request.user).order_by("-date")
 
 
+# journal book details view
+class JournalBookDetailsView(LoginRequiredMixin, DeleteView):
+    login_url = "/login"
+    model = JournalBook
+    template_name = "journal/journal-details.html"
+    context_object_name = "journal_details"
+
+
+    def dispatch(self, request, *args, **kwargs):
+        journal_id = self.kwargs["pk"]
+
+        self.journal_book = get_object_or_404(JournalBook, pk=int(journal_id), business=self.request.user)
+
+        return super().dispatch(request, *args, **kwargs)
+
+
+    def get_object(self, queryset = None):
+        return self.journal_book
     
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data()
+
+        journals = JournalTransaction.objects.filter(
+            business=self.request.user,
+            journal=self.journal_book,
+        ).order_by("date", "id")
+
+        context["journals"] = journals
+
+        
+
+        return context
+
